3e9a447ac1a5bf3f7bf2c9f8071fe1519cc63101
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-01-17 07:24:49 +0200

    v5.1.0

--- script.js
+++ script.js
@@ -39,18 +39,56 @@ function gotoAniyomi(link) {
 
 // manga sub pages
 
-function manga1(link) {
-  location.href = "/pages/manga.html";
-}
-function manga2(link) {
-  location.href = "/pages/manga-sub-pages/manga2.html";
-}
-function manga3(link) {
-  location.href = "/pages/manga-sub-pages/manga3.html";
-}
-function manga4(link) {
-  location.href = "/pages/manga-sub-pages/manga4.html";
-}
+// function manga1(link) {
+//   location.href = "/pages/manga.html";
+// }
+// function manga2(link) {
+//   location.href = "/pages/manga-sub-pages/manga2.html";
+// }
+// function manga3(link) {
+//   location.href = "/pages/manga-sub-pages/manga3.html";
+// }
+// function manga4(link) {
+//   location.href = "/pages/manga-sub-pages/manga4.html";
+// }
+
+const mangaContaner = document.querySelector(".manga__contaner");
+const btnpPage1 = document.querySelector("#btnpage1");
+const btnpPage2 = document.querySelector("#btnpage2");
+const btnpPage3 = document.querySelector("#btnpage3");
+const btnpPage4 = document.querySelector("#btnpage4");
+
+btnpPage1.addEventListener("click", function () {
+  console.log("btn1");
+  mangaContaner.classList.remove("manga2");
+  mangaContaner.classList.remove("manga3");
+  mangaContaner.classList.remove("manga4");
+  mangaContaner.classList.add("manga1");
+});
+
+btnpPage2.addEventListener("click", function () {
+  console.log("btn2");
+  mangaContaner.classList.remove("manga1");
+  mangaContaner.classList.remove("manga3");
+  mangaContaner.classList.remove("manga4");
+  mangaContaner.classList.add("manga2");
+});
+btnpPage3.addEventListener("click", function () {
+  console.log("btn3");
+  mangaContaner.classList.remove("manga1");
+  mangaContaner.classList.remove("manga2");
+  mangaContaner.classList.remove("manga4");
+  mangaContaner.classList.add("manga3");
+});
+btnpPage4.addEventListener("click", function () {
+  console.log("btn4");
+  mangaContaner.classList.remove("manga1");
+  mangaContaner.classList.remove("manga2");
+  mangaContaner.classList.remove("manga3");
+  mangaContaner.classList.add("manga4");
+});
+
+////
 
 //HTML sub pages
 
@@ -84,3 +122,5 @@ function section9(link) {
 function section10(link) {
   location.href = "#section3";
 }
+
+// manga single page app
