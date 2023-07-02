a35fe43dc98e8ecd35be27b7f32c89c6ae4d5e1e
Alexander EG#9072 <116421926+AlexEG@users.noreply.github.com>
2023-07-02 19:35:20 +0300

    v7.4.10
    <StatisticsSection />
    add Links
    freecodecamp
    codewars
    GitHub

--- src/components/Statistics-section/StatisticsSection.jsx
+++ src/components/Statistics-section/StatisticsSection.jsx
@@ -19,32 +19,62 @@ function StatisticsSection() {
           href="https://wakatime.com/@fbdfdc0f-d449-43dc-8090-ced03a22fe8c"
           rel="noreferrer"
           target="_blank"
+          title="WakaTime"
         >
           <img
             src="https://wakatime.com/badge/user/fbdfdc0f-d449-43dc-8090-ced03a22fe8c.svg"
             alt="Total time coded since Dec 22 2022"
           />
         </a>
-        <img
-          alt="freeCodeCamp points"
-          src="https://img.shields.io/freecodecamp/points/fcc4dcee3ab-a388-4f70-9ad9-0ac52ed81dc7?logo=freecodecamp&logoColor=%2349f3f2&color=%230a0a49&style=plastic"
-        />
-        <img
-          alt="codewars"
-          src="https://www.codewars.com/users/AlexEG/badges/micro"
-        />
-        <img
-          alt="GitHub followers"
-          src="https://img.shields.io/github/followers/AlexEG?logo=github&style=plastic"
-        />
-        <div className="h-5 rounded-sm flex overflow-hidden cursor-default">
-          <span className=" bg-slate-700 font-semibold text-slate-200 text-sm px-2">
-            GitHub public Repos
-          </span>
-          <span className="bg-slate-900 px-2 text-sm text-slate-50 font-bold text-center">
-            {publicReposNum}
-          </span>
-        </div>
+        <a
+          href="https://www.freecodecamp.org/fcc4dcee3ab-a388-4f70-9ad9-0ac52ed81dc7"
+          rel="noreferrer"
+          target="_blank"
+          title="FreeCodeCamp.org"
+        >
+          <img
+            alt="freeCodeCamp points"
+            src="https://img.shields.io/freecodecamp/points/fcc4dcee3ab-a388-4f70-9ad9-0ac52ed81dc7?logo=freecodecamp&logoColor=%2300002f&color=%230a0a49"
+          />
+        </a>
+        <a
+          href="https://www.codewars.com/users/AlexEG"
+          rel="noreferrer"
+          target="_blank"
+          title="codewars"
+        >
+          <img
+            alt="codewars"
+            src="https://www.codewars.com/users/AlexEG/badges/micro"
+          />
+        </a>
+        <a
+          href="https://github.com/AlexEG"
+          rel="noreferrer"
+          target="_blank"
+          title="GitHub"
+        >
+          <img
+            alt="GitHub followers"
+            src="https://img.shields.io/github/followers/AlexEG?logo=github&"
+          />
+        </a>
+
+        <a
+          href="https://github.com/AlexEG?tab=repositories"
+          rel="noreferrer"
+          target="_blank"
+          title="GitHub Repos"
+        >
+          <div className="h-5 rounded-sm flex overflow-hidden cursor-default">
+            <span className=" bg-slate-700 font-semibold text-slate-200 text-sm px-2">
+              GitHub public Repos
+            </span>
+            <span className="bg-slate-900 px-2 text-sm text-slate-50 font-bold text-center">
+              {publicReposNum}
+            </span>
+          </div>
+        </a>
       </div>
 
       {/* ------------------------------------- */}
