447e50ee89827ca382b314fe1fe96960b4337d88
AlexEG <116421926+AlexEG@users.noreply.github.com>
2024-06-06 14:43:35 +0300

    v8.3.1

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,29 +1,29 @@
 import { defineCollection, z } from "astro:content";
 
-const blog = defineCollection({
-  type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
-const ossu = defineCollection({
-  type: "content",
-  // Type-check frontmatter using a schema
-  schema: z.object({
-    title: z.string(),
-    description: z.string(),
-    // Transform string to Date object
-    pubDate: z.coerce.date(),
-    updatedDate: z.coerce.date().optional(),
-    heroImage: z.string().optional(),
-  }),
-});
+// const blog = defineCollection({
+//   type: "content",
+//   // Type-check frontmatter using a schema
+//   schema: z.object({
+//     title: z.string(),
+//     description: z.string(),
+//     // Transform string to Date object
+//     pubDate: z.coerce.date(),
+//     updatedDate: z.coerce.date().optional(),
+//     heroImage: z.string().optional(),
+//   }),
+// });
+// const ossu = defineCollection({
+//   type: "content",
+//   // Type-check frontmatter using a schema
+//   schema: z.object({
+//     title: z.string(),
+//     description: z.string(),
+//     // Transform string to Date object
+//     pubDate: z.coerce.date(),
+//     updatedDate: z.coerce.date().optional(),
+//     heroImage: z.string().optional(),
+//   }),
+// });
 const javascript = defineCollection({
   type: "content",
   // Type-check frontmatter using a schema
@@ -38,4 +38,4 @@ const javascript = defineCollection({
   }),
 });
 
-export const collections = { blog, ossu, javascript };
+export const collections = { javascript };
