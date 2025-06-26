a1342fa7161d3bbd3c86faea85e5a3a4e440c926
AlexEG <aviva.youtupe@gmail.com>
2025-06-26 16:19:37 +0300

    Revert "V8.5.0"
    This reverts commit 5ae8a730135ae8ce613d407d93908d283df06cc5.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,11 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-import tailwind from "@astrojs/tailwind";
 
-import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexeg.github.io",
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind()],
 });
--- src/content/linux/i3 on Debain 12.md
+++
@@ -1,8 +0,0 @@
----
-title: i3 on Debain 12
-description: ""
-pubDate: May 18 2025
-updateDate: May 20 2025
-tags: "Debian-12,i3 , window-manager"
-heroImage: /blog-placeholder-3.jpg
----
