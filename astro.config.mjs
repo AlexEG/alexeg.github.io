94ad09ce669eb91aa802346387404442cd1989c3
AlexEG <116421926+AlexEG@users.noreply.github.com>
2025-08-01 22:57:12 +0300

    v8.5.2
    Add React and shadcn/ui integration with Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import tailwind from "@astrojs/tailwind";
 
+import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://alexeg.github.io",
-  integrations: [mdx(), sitemap(), tailwind()],
+  integrations: [mdx(), sitemap(), tailwind(), react()]
 });
